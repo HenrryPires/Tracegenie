0eb43dd047a4b2b1902ca3ffd4c8eb0c5feb1401
Henrry Pires <HenrryPires@gmail.com>
2019-01-09 23:11:20 +0000

    Update test.js
    new stuff

--- test.js
+++ test.js
@@ -12,7 +12,7 @@ async function login(driver) {
     await driver.get('https://tracegenie.com/amember4/amember/login');
 
     await driver.findElement(webdriver.By.id('amember-login')).sendKeys('henrrypires@gmail.com');
-    await driver.findElement(webdriver.By.id('amember-pass')).sendKeys('Nelson796\n');
+    await driver.findElement(webdriver.By.id('amember-pass')).sendKeys('\n');
 
     await driver.wait(webdriver.until.elementLocated(webdriver.By.id('menu-member')), 2000);
 }
@@ -25,9 +25,9 @@ async function getData(driver, surname, area, page) {
 
     for (var i = 0; i < tables.length; i++) { 
         var addressArray = tables[i].rows[1].cells[0].innerText;
-        var address = addressArray.split('\\n').filter(value => value != '' && !value.startsWith('Years at address')).reduce((current, newValue) => current + ' ' + newValue)
+        var address = addressArray.split('\\n').filter(value => value != '' && !value.startsWith('Years at address'));
 
-        var entry = {name: tables[i].rows[0].cells[1].innerText.replace(/\\s\\s+/g, ' ').trim(), address: address};
+        var entry = {name: tables[i].rows[0].cells[1].innerText.replace(/\\s\\s+/g, ' ').trim(), postcode: address.pop().trim(), city: address.pop().trim(), address: address.reduce((current, newValue) => current + ' ' + newValue)};
         data.push(entry);
     }
     
@@ -45,7 +45,7 @@ async function getData(driver, surname, area, page) {
 }
 
 async function getNext(driver) {
-    return await driver.executeScript("return document.evaluate('/html/body/font/font/a', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue");
+    return await driver.executeScript("return document.getElementsByTagName('table').length");
 }
 
 class Requester {
@@ -62,28 +62,32 @@ class Requester {
 
         let json = await getData(driver, surname, area, page);
 
-        let next = await getNext(driver);
-
-        while(next !== null) {
+        let next = true;
+        
+        while(next) {
             page = page + 10;
             
             const newData = await getData(driver, surname, area, page);
             json = json.concat(newData);
-
-            next = await getNext(driver);
+            next = await getNext(driver) > 0;
         }
         driver.close();
+        
+        try {
+            var fields = Object.keys(json[0])
+            var csv = json.map(row => {
+                return fields.map(fieldName => {
+                    return JSON.stringify(row[fieldName]);
+                }).join(',') + '\n';
+            });
 
-        var fields = Object.keys(json[0])
-        var csv = json.map(row => {
-            return fields.map(fieldName => {
-                return JSON.stringify(row[fieldName]);
-            }).join(',') + '\n';
-        });
+            csv.unshift(fields.join(',') + '\n') // add header column
 
-        csv.unshift(fields.join(',') + '\n') // add header column
-
-        await fs.writeFile('data.csv', csv.reduce((a,b) => a = a + b));
+            await fs.writeFile(`${surname}.csv`, csv.reduce((a,b) => a = a + b));
+        } 
+        catch (error) {
+            log(`no data for ${surname}`)
+        }
     }
 }
 
