76af7acf3ae7c380c2668b6a84517ef5110a31f2
Henrry Pires <HenrryPires@gmail.com>
2019-01-09 23:12:34 +0000

    Update names.js
    new stuff

--- names.js
+++ names.js
@@ -1,9 +1,31 @@
+
 const Requester = require('./test.js');
+var fs = require('fs');
 
 async function startProcesses() {
-
     const r = new Requester();
-    r.request('Silva', 'Portsmouth');
+    let names = [];
+
+    let data = await fs.readFileSync('names.csv', 'utf8');
+    names = data.split(',');
+
+    //get the data
+    for(let i = 0; i < names.length; i++) {
+        await r.request(names[i], 'Po');
+    };
+    
+    //concat the files
+    for(let i = 0; i < names.length; i++) {
+        try {
+            
+            data = await fs.readFileSync(`${names[i]}.csv`, 'utf8');
+            data = data + '\n'
+            await fs.writeFile(`AllData.csv`, data, options = {flag: 'a'});    
+            
+        } catch (error) {
+            console.log(`no data for ${names[i]}.csv`)
+        }
+    };
 }
 
 startProcesses();
